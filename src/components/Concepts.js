46695597a0333b5f4240c4253a08890cd1806458
PRO-GRAM-MER <ankitbaage3@gmail.com>
2023-07-12 13:19:56 +0530

    first commit

---
+++ src/components/Concepts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Concepts.css";
+
+export const Concepts = (props) => {
+  return (
+    <ul id="concepts">
+      {props.concepts.map((concept) => (
+        <li className="concept" key={concept.title}>
+          <img src={concept.image} alt={concept.title} />
+          <h2>{concept.title}</h2>
+          <p>{concept.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
